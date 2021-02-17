fafaab10092d9e703bcaf6ca19f5cd005792cf9e
namtong <namtong@omt.com.vn>
2021-02-17 22:43:32 +0700

    Add all my files

---
+++ config/services.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'github' => [
+        'client_id' => '24fbd0edd46019058da7', //Github API
+        'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', //Github Secret
+        'redirect' => 'http://localhost:8000/login/github/callback',
+     ],
+     'google' => [
+        'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', //Google API
+        'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', //Google Secret
+        'redirect' => 'http://localhost:8000/login/google/callback',
+     ],
+     'facebook' => [
+        'client_id' => '243548920679623', //Facebook API
+        'client_secret' => '9ea42ea8e912e9025123a3d45e02aa57', //Facebook Secret
+        'redirect' => 'https://motiongym.abc/login/facebook/callback',
+     ],
+
+];
